a0aa138a460bb4dd7d1b9d0c98d1b7305309df57
NanoSector <rick@nanosector.nl>
2023-04-14 23:27:12 +0200

    Add PlaintextVersionDriver

--- src/Domain/Endpoint/Driver/DriverEnum.php
+++ src/Domain/Endpoint/Driver/DriverEnum.php
@@ -10,6 +10,7 @@ declare(strict_types = 1);
 namespace Domain\Endpoint\Driver;
 
 use Domain\Endpoint\Driver\Bazarr\BazarrDriver;
+use Domain\Endpoint\Driver\GenericPlaintextVersion\GenericPlaintextVersionDriver;
 use Domain\Endpoint\Driver\NextcloudOcs\NextcloudOcsDriver;
 use Domain\Endpoint\Driver\PlexMediaServer\PlexMediaServerDriver;
 use Domain\Endpoint\Driver\Sonarr\SonarrDriver;
@@ -17,6 +18,7 @@ use Domain\Endpoint\Driver\Sonarr\SonarrDriver;
 enum DriverEnum: string
 {
     case BazarrDriver = BazarrDriver::class;
+    case GenericPlaintextVersionDriver = GenericPlaintextVersionDriver::class;
     case NextcloudOcsDriver = NextcloudOcsDriver::class;
     case PlexMediaServerDriver = PlexMediaServerDriver::class;
     case Sonarr = SonarrDriver::class;
@@ -30,6 +32,7 @@ enum DriverEnum: string
     {
         return match ($this) {
             self::BazarrDriver => 'Bazarr',
+            self::GenericPlaintextVersionDriver => 'Plain text version (generic)',
             self::NextcloudOcsDriver => 'Nextcloud (OCS)',
             self::PlexMediaServerDriver => 'Plex Media Server',
             self::Sonarr => 'Sonarr & co (Radarr, Prowlarr, etc.)'
---
+++ src/Domain/Endpoint/GenericResponse/PlaintextVersionResponse.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Copyright (c) 2023 NanoSector & Orchestra contributors
+ *
+ * This source code is licensed under the MIT license. See LICENSE for details.
+ */
+
+declare(strict_types = 1);
+
+namespace Domain\Endpoint\GenericResponse;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Domain\Endpoint\Driver\AbstractDriverResponse;
+use Domain\Metric\VersionMetric;
+use InvalidArgumentException;
+
+readonly class PlaintextVersionResponse extends AbstractDriverResponse
+{
+
+    /**
+     * @throws InvalidArgumentException when version string does not look like a version string
+     */
+    public function __construct(string $product, string $version)
+    {
+        if (strlen($version) > 20) {
+            throw new InvalidArgumentException('Version string is very long; is this really a version string?');
+        }
+
+        // Strip any leading v's (e.g. v1.2.0)
+        $version = ltrim($version, "v");
+
+        parent::__construct(
+            new ArrayCollection([
+                new VersionMetric($product, $version)
+            ])
+        );
+    }
+
+}
