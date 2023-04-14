eb3ca922467cf076ed4ca9044d834b52d3c95c10
NanoSector <rick@nanosector.nl>
2023-04-14 22:52:28 +0200

    Add Bazarr and Sonarr drivers

--- src/Domain/Endpoint/Driver/DriverEnum.php
+++ src/Domain/Endpoint/Driver/DriverEnum.php
@@ -9,13 +9,17 @@ declare(strict_types = 1);
 
 namespace Domain\Endpoint\Driver;
 
+use Domain\Endpoint\Driver\Bazarr\BazarrDriver;
 use Domain\Endpoint\Driver\NextcloudOcs\NextcloudOcsDriver;
 use Domain\Endpoint\Driver\PlexMediaServer\PlexMediaServerDriver;
+use Domain\Endpoint\Driver\Sonarr\SonarrDriver;
 
 enum DriverEnum: string
 {
+    case BazarrDriver = BazarrDriver::class;
     case NextcloudOcsDriver = NextcloudOcsDriver::class;
     case PlexMediaServerDriver = PlexMediaServerDriver::class;
+    case Sonarr = SonarrDriver::class;
 
     public function getContainerDefinition(): string
     {
@@ -25,8 +29,10 @@ enum DriverEnum: string
     public function getFriendlyName(): string
     {
         return match ($this) {
+            self::BazarrDriver => 'Bazarr',
             self::NextcloudOcsDriver => 'Nextcloud (OCS)',
             self::PlexMediaServerDriver => 'Plex Media Server',
+            self::Sonarr => 'Sonarr & co (Radarr, Prowlarr, etc.)'
         };
     }
 }
---
+++ src/Domain/Endpoint/Driver/Sonarr/SonarrResponseParser.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Copyright (c) 2023 NanoSector & Orchestra contributors
+ *
+ * This source code is licensed under the MIT license. See LICENSE for details.
+ */
+
+declare(strict_types = 1);
+
+namespace Domain\Endpoint\Driver\Sonarr;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Domain\Exception\EndpointExecutionFailedException;
+use Domain\Metric\Parser\Builder\ParserBuilder;
+use Domain\Metric\Parser\ResponseStructure;
+use Symfony\Contracts\HttpClient\Exception\ExceptionInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+readonly class SonarrResponseParser
+{
+
+    public function __construct(private ParserBuilder $builder)
+    {
+    }
+
+    /**
+     * @throws EndpointExecutionFailedException
+     */
+    public function parse(ResponseInterface $response): SonarrDriverResponse
+    {
+        try {
+            $payload = $response->toArray();
+        } catch (ExceptionInterface $e) {
+            throw new EndpointExecutionFailedException('Payload failed to decode', 0, $e);
+        }
+
+        $product = $payload['appName'] ?? 'Sonarr';
+
+        $structure = new ResponseStructure([
+            'version' => $this->builder->metric()->version($product),
+        ]);
+
+        $metrics = $structure->parseMetrics($payload);
+
+        return new SonarrDriverResponse($response, new ArrayCollection($metrics));
+    }
+}
