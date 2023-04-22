e808954678e48d5b4dcb399962500f4ab8e932a8
NanoSector <rick@nanosector.nl>
2023-04-22 21:54:08 +0200

    Add ApplyToEachNode

---
+++ src/Domain/Metric/Parser/Node/ApplyToEachNode.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * Copyright (c) 2023 NanoSector & Orchestra contributors
+ *
+ * This source code is licensed under the MIT license. See LICENSE for details.
+ */
+
+declare(strict_types = 1);
+
+namespace Orchestra\Domain\Metric\Parser\Node;
+
+use Closure;
+
+readonly class ApplyToEachNode extends AbstractParserControlStructure
+{
+    public function __construct(
+        protected Closure $consumer
+    ) {
+    }
+
+    public function parse(array|string $value): array
+    {
+        if (!is_array($value)) {
+            $value = [$value];
+        }
+
+        foreach ($value as $key => $item) {
+            $value[$key] = call_user_func($this->consumer, $item, $key);
+        }
+
+        return $value;
+    }
+}
